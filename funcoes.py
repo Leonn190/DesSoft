ec65e56fbe3ea89c1b265db2480ff8ec17022b3b
Leonn190 <euleonsoto@gmail.com>
2025-05-03 20:07:46 -0300

    nova funçao

--- funcoes.py
+++ funcoes.py
@@ -15,3 +15,10 @@ def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
     dado = dados_no_estoque.pop(dado_para_remover)
     dados_rolados.append(dado)
     return [dados_rolados, dados_no_estoque]
+
+def calcula_pontos_regra_simples(dados):
+    pontos = {i: 0 for i in range(1, 7)}
+    for d in dados:
+        if 1 <= d <= 6:
+            pontos[d] += d
+    return pontos
