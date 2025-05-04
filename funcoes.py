de814cd56b244358c3ff6662c81467effcf59207
Leonn190 <euleonsoto@gmail.com>
2025-05-03 23:11:06 -0300

    mais uma

--- funcoes.py
+++ funcoes.py
@@ -97,3 +97,15 @@ def calcula_pontos_quina(dados):
         if quantidade >= 5:
             return 50
     return 0
+
+def calcula_pontos_regra_avancada(dados):
+    # Dicionário com as pontuações de todas as regras avançadas
+    pontos = {
+        'cinco_iguais': calcula_pontos_quina(dados),
+        'full_house': calcula_pontos_full_house(dados),
+        'quadra': calcula_pontos_quadra(dados),
+        'sem_combinacao': calcula_pontos_soma(dados),
+        'sequencia_alta': calcula_pontos_sequencia_alta(dados),
+        'sequencia_baixa': calcula_pontos_sequencia_baixa(dados)
+    }
+    return pontos
